2dabe7051120f3dab6a19d5e4d92a71b5972d1e4
Polaris Void <65972401+poevoid@users.noreply.github.com>
2026-04-11 00:57:31 -0500

    Delete Arduboy2GBA directory

--- Arduboy2GBA/runtime/audio.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUDIO_H
-#define AUDIO_H
-
-void audio_tone(int freq, int duration);
-void audio_play_score(const unsigned char* score);
-void audio_stop_score();
-void audio_update();
-
-#endif
--- Arduboy2GBA/runtime/input.h
+++
@@ -1,9 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-#include <gba_types.h>
-
-u16 input_poll();
-int input_pressed(u16 key);
-
-#endif
